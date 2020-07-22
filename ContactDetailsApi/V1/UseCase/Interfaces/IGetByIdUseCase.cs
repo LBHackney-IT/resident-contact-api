b3b5a8fb13407b936198f7e1a4d34aeab3ce7fbd
humulla <humairaa.mulla@hackney.gov.uk>
2020-07-22 16:55:14 +0100

    renamed project to resident-contact-api

--- ContactDetailsApi/V1/UseCase/Interfaces/IGetByIdUseCase.cs
+++
@@ -1,9 +0,0 @@
-using ContactDetailsApi.V1.Boundary.Response;
-
-namespace ContactDetailsApi.V1.UseCase.Interfaces
-{
-    public interface IGetByIdUseCase
-    {
-        ResponseObject Execute(int id);
-    }
-}
