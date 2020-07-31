630f077fc1e4df41ea69a895c1124ad063a1bd83
elcorbs <emma@madetech.com>
2020-07-31 12:02:12 +0100

    Add development database and run db migrations in pipeline

--- Makefile
+++ Makefile
@@ -18,6 +18,11 @@ shell:
 test:
 	docker-compose up test-database & docker-compose build base-api-test && docker-compose up base-api-test
 
+.PHONY: migrate-local-test-database
+migrate-test-database:
+	-dotnet tool install -g dotnet-ef
+	cd ResidentContactApi && CONNECTION_STRING="Host=127.0.0.1;Port=5432;Username=postgres;Password=mypassword;Database=testdb" dotnet ef database update
+
 .PHONY: lint
 lint:
 	-dotnet tool install -g dotnet-format
--- terraform/development/main.tf
+++ terraform/development/main.tf
@@ -1,6 +1,6 @@
-# INSTRUCTIONS: 
-# 1) ENSURE YOU POPULATE THE LOCALS 
-# 2) ENSURE YOU REPLACE ALL INPUT PARAMETERS, THAT CURRENTLY STATE 'ENTER VALUE', WITH VALID VALUES 
+# INSTRUCTIONS:
+# 1) ENSURE YOU POPULATE THE LOCALS
+# 2) ENSURE YOU REPLACE ALL INPUT PARAMETERS, THAT CURRENTLY STATE 'ENTER VALUE', WITH VALID VALUES
 # 3) YOUR CODE WOULD NOT COMPILE IF STEP NUMBER 2 IS NOT PERFORMED!
 # 4) ENSURE YOU CREATE A BUCKET FOR YOUR STATE FILE AND YOU ADD THE NAME BELOW - MAINTAINING THE STATE OF THE INFRASTRUCTURE YOU CREATE IS ESSENTIAL - FOR APIS, THE BUCKETS ALREADY EXIST
 # 5) THE VALUES OF THE COMMON COMPONENTS THAT YOU WILL NEED ARE PROVIDED IN THE COMMENTS
@@ -14,8 +14,8 @@ provider "aws" {
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 locals {
-  application_name = "resident contact api"
-   parameter_store = "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter"
+  application_name = "Resident Contact API"
+  parameter_store = "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter"
 }
 
 terraform {
@@ -23,25 +23,26 @@ terraform {
     bucket  = "terraform-state-development-apis"
     encrypt = true
     region  = "eu-west-2"
-    key     = services/resident-contact-api/state
+    key     = "services/resident-contact-api/state"
   }
 }
 
 /*    POSTGRES SET UP    */
-data "aws_vpc" "development_vpc" {  
-  tags = {    
-    Name = "vpc-development-apis-development"  
-    }
-}
-data "aws_subnet_ids" "development_private_subnets" {  
-  vpc_id = data.aws_vpc.development_vpc.id  
-  filter {    
-    name   = "tag:Type"    
-    values = ["private"]  
+data "aws_vpc" "development_vpc" {
+  tags = {
+    Name = "vpc-development-apis-development"
     }
 }
 
- data "aws_ssm_parameter" "resident_contact_postgres_db_password" {
+data "aws_subnet_ids" "development_private_subnets" {
+  vpc_id = data.aws_vpc.development_vpc.id
+  filter {
+    name   = "tag:Type"
+    values = ["private"]
+    }
+}
+
+ data "aws_ssm_parameter" "resident_contact_postgres_password" {
    name = "/resident-contact-api/development/postgres-password"
  }
 
@@ -55,12 +56,12 @@ module "postgres_db_development" {
   vpc_id = data.aws_vpc.development_vpc.id
   db_engine = "postgres"
   db_engine_version = "11.1"
-  db_identifier = "resident-contact-dev-db"
+  db_identifier = "resident-contact-development-db"
   db_instance_class = "db.t2.micro"
-  db_name = "resident-contact_dev"
+  db_name = "resident-contact-development"
   db_port  = 5302
   db_username = data.aws_ssm_parameter.resident_contact_postgres_username.value
-  db_password = data.aws_ssm_parameter.resident_contact_postgres_db_password.value
+  db_password = data.aws_ssm_parameter.resident_contact_postgres_password.value
   subnet_ids = data.aws_subnet_ids.development_private_subnets.ids
   db_allocated_storage = 20
   maintenance_window ="sun:10:00-sun:10:30"
