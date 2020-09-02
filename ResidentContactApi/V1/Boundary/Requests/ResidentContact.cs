ad5a90edc3f03b08a76ef392784b670cf2dd4230
LBHBomotoso <46450498+LBHBomotoso@users.noreply.github.com>
2020-09-02 09:56:18 +0100

    fix annotation

--- ResidentContactApi/V1/Boundary/Requests/ResidentContact.cs
+++ ResidentContactApi/V1/Boundary/Requests/ResidentContact.cs
@@ -1,8 +1,9 @@
-using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
 
 namespace ResidentContactApi.V1.Boundary.Requests
 {
+    [DataContract]
     public class ResidentContact
     {
         [Required]
@@ -10,24 +11,24 @@ namespace ResidentContactApi.V1.Boundary.Requests
 
         //Value eg. a phone/mobile number, e-mail etc.
         [Required]
-        [BindProperty(Name = "value")]
+        [DataMember(Name = "value")]
         public string ContactValue { get; set; }
 
         [Required]
-        [BindProperty(Name = "active")]
+        [DataMember(Name = "active")]
         public bool IsActive { get; set; }
 
         [Required]
-        [BindProperty(Name = "default")]
+        [DataMember(Name = "default")]
         public bool IsDefault { get; set; }
 
         //Integer values used to map to stored lookup values/enums
         [Required]
-        [BindProperty(Name = "typeId")]
+        [DataMember(Name = "typeId")]
         public int ContactTypeLookupId { get; set; }
 
         //Nullable or Integer values used to map to stored lookup values/enums
-        [BindProperty(Name = "subTypeId")]
+        [DataMember(Name = "subTypeId")]
         public int? ContactSubTypeLookupId { get; set; }
     }
 }
