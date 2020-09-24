3b13647cb49b7e1ae1198995cfb61a785c7d4043
humulla <humairaa.mulla@hackney.gov.uk>
2020-09-24 10:41:48 +0100

    solved issues with build

--- ResidentContactApi/V1/Gateways/IResidentGateway.cs
+++ ResidentContactApi/V1/Gateways/IResidentGateway.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using ResidentContactApi.V1.Domain;
 using ResidentContactApi.V1.Boundary.Requests;
 using System;
+using ResidentContactApi.V1.Boundary.Response;
 
 namespace ResidentContactApi.V1.Gateways
 {
@@ -11,7 +12,8 @@ namespace ResidentContactApi.V1.Gateways
         List<ResidentDomain> GetResidents(int limit, int cursor, string firstName, string lastName);
         ResidentDomain GetResidentById(int id);
         int? InsertResidentContactDetails(int? residentId, string nccContactId, ContactDetailsDomain contactDetails);
-
+        InsertResidentResponse InsertNewResident(InsertResidentRequest request);
+        void InsertExternalReferences(InsertResidentRequest request, int residentId);
         void InvalidateContactDetailsRecord(int? id);
     }
 }
