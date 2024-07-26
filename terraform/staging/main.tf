00fbfff49693a49284857a272d5d2da70afae95c
Liudvikas Taluntis <43747286+Duslerke@users.noreply.github.com>
2024-07-26 11:32:50 +0100

    Remove unused staging terraform local parameters.
    Co-authored-by: Miles Alford <43789512+Miles-Alford@users.noreply.github.com>

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -2,12 +2,6 @@ provider "aws" {
   region  = "eu-west-2"
   version = "~> 2.0"
 }
-data "aws_caller_identity" "current" {}
-data "aws_region" "current" {}
-locals {
-   application_name = "resident contact api"
-   parameter_store = "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter"
-}
 
 terraform {
   backend "s3" {
